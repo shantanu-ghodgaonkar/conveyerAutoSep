684f08e8b9c07fed8617038d47dd23d78d541823
shantanu-ghodgaonkar <shantanu.ghodgaonkar@gmail.com>
2024-02-28 13:29:32 -0500

    Modified barcode scanner code to ensure proper barcode is being scanned

--- main/main.ino
+++ main/main.ino
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <ParallaxLCD.h>
 #include <Servo.h>
+#include <Regexp.h>
 
 // Pins
 #define PINGSENSOR 7  // PING Sensor on Pin 7
@@ -18,7 +19,7 @@
 
 // Servo motor Constants
 #define BOTSERVOHOME 90   // Bottom Servo Home Angle
-#define TOPSERVOHOME 94   // Top Servo Home Angle
+#define TOPSERVOHOME 105   // Top Servo Home Angle
 #define SERVOOFFSET 30    // Servo Offset Angle
 
 SoftwareSerial barcodeScanner(BCSTX, BCSRX); // Declare Barcode Scanner as a serial device
@@ -31,36 +32,6 @@ String tempBarcode = "NONE"; // Variable to store temporarily scanned barcode fo
 unsigned long startMillis = 0; // Variable to store the millis() when scanBarcode() is called
 unsigned long interval = 4000; // Variable to store the interval for which the barcode is to be switched on
 unsigned long currentMillis = 0; // Variable to store the current value of millis() to check how long the barcode scanner has been switched on for
-
-void setup() {
-  botServo.attach(BOTSERVO); // Attach bottom servo to pin 9
-  topServo.attach(TOPSERVO); // Attach top servo to pin 10
-  botServo.write(BOTSERVOHOME); // Set bottom servo to home configuration
-  topServo.write(TOPSERVOHOME); // Set top servo to home configuration
-  pinMode(DCMRELAY, OUTPUT); // Set DC motor Relay pin as output
-  digitalWrite(DCMRELAY, HIGH); // Switch on DC motor
-  Serial.begin(9600); // Start serial communication
-  // barcodeScanner.begin(115200);
-}
-
-void loop() {
-  lcd.empty(); // Clear LCD Screen
-  distance = runPINGSensor(); // Get PING Sensor reading
-  if(distance < 10){ // If distance is less than 10 cm, package is in postion to be scanned
-    digitalWrite(DCMRELAY, LOW); // Switch off motor; switch on barcode scanner
-    scanBarcode(); // Scan barcode
-    lcdDisplayBarcode(); // Display scanned barcode on LCD
-    digitalWrite(DCMRELAY, HIGH); // Switch on DC motor; switch off barcode scanner
-    delay(1000); // Wait for package to fall on turntable
-    if(barcode.substring(0,3) == "212") botServo.write(BOTSERVOHOME - SERVOOFFSET); // Check for first area and rotate servos accordingly
-    else if (barcode.substring(0,3) == "929") botServo.write(BOTSERVOHOME + SERVOOFFSET); // Check for second area and rotate servos accordingly
-    else if (barcode.substring(0,3) == "718") topServo.write(TOPSERVOHOME + SERVOOFFSET); // Check for third area and rotate servos accordingly
-    delay(1000); // Allow 1s of delay for package to fall into right box
-    botServo.write(BOTSERVOHOME); // Bring bottom servo back to home
-    topServo.write(TOPSERVOHOME); // Bring top servo back to home
-  }
-}
-
 
 long runPINGSensor() { // This function is responsible to get sensordata from PING sensor 
  // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
@@ -88,11 +59,15 @@ void scanBarcode() { // This function is responsible to get the scanned barcode 
     if (barcodeScanner.available()) // Check if there is Incoming Data in the Serial Buffer.
     { Serial.println("Barcode scanner has data!"); 
       while (barcodeScanner.available()) // Keep reading Byte by Byte from the Buffer till the Buffer is empty
-      {tempBarcode = barcodeScanner.readString();} // Store current read barcode in a temporary variable
+      {tempBarcode = barcodeScanner.readString();
+      // barcode = barcodeScanner.readString();
+      } // Store current read barcode in a temporary variable
     }
-    if(tempBarcode != barcode) { // Check if same barcode has not been read twice
-      barcode = tempBarcode; // Update barcode variable
-      break; // Exit while loop
+    if(tempBarcode[3] == '-'){
+      if(tempBarcode != barcode) { // Check if same barcode has not been read twice
+        barcode = tempBarcode; // Update barcode variable
+        break; // Exit while loop
+      }
     }
   }
   Serial.println(barcode); // Print read barcode
@@ -105,6 +80,37 @@ void lcdDisplayBarcode() {
   lcd.empty(); // Clear LCD Screen
   lcd.print("Scanned Barcode: "); // Display scanned barcode on LCD
   lcd.print(barcode);
+  delay(1500);
   lcd.end();
 }
+
+void setup() {
+  botServo.attach(BOTSERVO); // Attach bottom servo to pin 9
+  topServo.attach(TOPSERVO); // Attach top servo to pin 10
+  botServo.write(BOTSERVOHOME); // Set bottom servo to home configuration
+  topServo.write(TOPSERVOHOME); // Set top servo to home configuration
+  pinMode(DCMRELAY, OUTPUT); // Set DC motor Relay pin as output
+  digitalWrite(DCMRELAY, HIGH); // Switch on DC motor
+  Serial.begin(9600); // Start serial communication
+  // barcodeScanner.begin(115200);
+}
+
+void loop() {
+  lcd.empty(); // Clear LCD Screen
+  distance = runPINGSensor(); // Get PING Sensor reading
+  if(distance < 10){ // If distance is less than 10 cm, package is in postion to be scanned
+    digitalWrite(DCMRELAY, LOW); // Switch off motor; switch on barcode scanner
+    scanBarcode(); // Scan barcode
+    lcdDisplayBarcode(); // Display scanned barcode on LCD
+    digitalWrite(DCMRELAY, HIGH); // Switch on DC motor; switch off barcode scanner
+    delay(1000); // Wait for package to fall on turntable
+    if(barcode.substring(0,3) == "212") botServo.write(BOTSERVOHOME - SERVOOFFSET); // Check for first area and rotate servos accordingly
+    else if (barcode.substring(0,3) == "929") botServo.write(BOTSERVOHOME + SERVOOFFSET); // Check for second area and rotate servos accordingly
+    else if (barcode.substring(0,3) == "718") topServo.write(TOPSERVOHOME - SERVOOFFSET); // Check for third area and rotate servos accordingly
+    delay(1000); // Allow 1s of delay for package to fall into right box
+    botServo.write(BOTSERVOHOME); // Bring bottom servo back to home
+    topServo.write(TOPSERVOHOME); // Bring top servo back to home
+  }
+}
+
 
